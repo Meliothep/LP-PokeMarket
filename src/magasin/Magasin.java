1f3a249940866a194e8e071f80b2d3aa179da5d7
brois <brois8572@gmail.com>
2022-09-23 11:47:25 +0200

    modifs stock (tous les tests passent pour moi) ;)

--- src/magasin/Magasin.java
+++ src/magasin/Magasin.java
@@ -2,10 +2,7 @@ package magasin;
 
 import magasin.exceptions.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Magasin implements iStock, iClientele, iPanier{
 
@@ -30,45 +27,54 @@ public class Magasin implements iStock, iClientele, iPanier{
     @Override
     public void referencerAuStock(iArticle nouvelArticle, int quantiteNouvelle)
             throws ArticleDejaEnStockException, QuantiteNegativeException {
-        // TODO
+        if(quantiteNouvelle < 0 ) throw new QuantiteNegativeException();
+        if(stock.containsKey(nouvelArticle)) throw new ArticleDejaEnStockException();
+        stock.put(nouvelArticle, quantiteNouvelle);
     }
 
 
     @Override
     public void reapprovisionnerStock(iArticle articleMaj, int quantiteAjoutee)
             throws ArticleHorsStockException, QuantiteNegativeOuNulleException {
-        // TODO
+        if(quantiteAjoutee <= 0) throw new QuantiteNegativeOuNulleException();
+        if(!stock.containsKey(articleMaj)) throw new ArticleHorsStockException();
+        stock.put(articleMaj,stock.get(articleMaj) + quantiteAjoutee);
     }
 
     @Override
     public int consulterQuantiteEnStock(iArticle articleRecherche) throws ArticleHorsStockException {
-        // TODO
-        return -1;
+        if(!stock.containsKey(articleRecherche)) throw new ArticleHorsStockException();
+        return stock.get(articleRecherche);
     }
 
     @Override
     public void retirerDuStock(int quantiteRetiree, iArticle articleMaj)
             throws ArticleHorsStockException, QuantiteNegativeOuNulleException, QuantiteEnStockInsuffisanteException {
-        // TODO
+        if(quantiteRetiree<=0) throw new QuantiteNegativeOuNulleException();
+        if(!stock.containsKey(articleMaj)) throw new ArticleHorsStockException();
+        if(quantiteRetiree>stock.get(articleMaj))throw new QuantiteEnStockInsuffisanteException();
+
+        stock.put(articleMaj, stock.get(articleMaj)-quantiteRetiree);
     }
 
 
     @Override
     public List<iArticle> listerArticlesEnStockParNom() {
-        // TODO
-        return null;
+        List<iArticle> articlesordreAlpha = new ArrayList<>(stock.keySet());
+        articlesordreAlpha.sort(iArticle.COMPARATEUR_NOM);
+        return articlesordreAlpha;
     }
 
     @Override
     public List<iArticle> listerArticlesEnStockParReference() {
-        // TODO
-        return null;
+        List<iArticle> articlesParRef = new ArrayList<>(stock.keySet());
+        articlesParRef.sort(iArticle.COMPARATEUR_REFERENCE);
+        return articlesParRef;
     }
 
     @Override
     public List<Map.Entry<iArticle, Integer>> listerStock() {
-        // TODO
-        return null;
+        return stock.entrySet().stream().sorted((o1, o2) -> iArticle.COMPARATEUR_NOM.compare(o1.getKey(), o2.getKey())).toList();
     }
 
     // iClientele
