316a8a73b0f30566aa140bfa28f4e2bb5c5309ab
Nicolas BERVOAS <nicolas.bervoas@etu.univ-nantes.fr>
2022-10-24 14:25:27 +0200

    progress

--- src/monapplication/components/HistoryModal.java
+++ src/monapplication/components/HistoryModal.java
@@ -12,29 +12,48 @@ import java.util.Map;
 
 public class HistoryModal extends JDialog {
 
-    private MainPanel context;
-    private JTabbedPane tabbedPane;
+    private final MainPanel context;
+    private final JTabbedPane tabbedPane;
 
-    public HistoryModal(MainPanel context){
+    public HistoryModal(MainPanel context) {
         super(MonApplication.frame(), true);
         this.context = context;
         tabbedPane = new JTabbedPane();
         add(tabbedPane);
-        setPreferredSize(new Dimension(500,500));
+        setPreferredSize(new Dimension(350, 500));
+        setResizable(false);
     }
 
-    private JPanel createCommandPannel(Commande commande){
-        return new JPanel();
+    private ScrollPane createCommandPannel(Commande commande) {
+        JPanel mainList = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        mainList.add(new JPanel(), gbc);
+        ScrollPane commandContainer = new ScrollPane();
+        commandContainer.add(mainList);
+        for (Map.Entry<iArticle, Integer> entry : commande.listerCommande()) {
+            addItem(mainList, entry);
+        }
+        return commandContainer;
+    }
+
+    private void addItem(JPanel mainList, Map.Entry<iArticle, Integer> entry) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        mainList.add(new CartItemPanel((PokemonArticle) entry.getKey(), entry.getValue()), gbc, 0);
     }
 
     public void update() {
         try {
             tabbedPane.removeAll();
             int count = 1;
-            for(Commande commande : MonApplication.magasin().listerCommandesTerminees(context.client())){
-                System.out.println(count);
-                tabbedPane.addTab("order " + count,createCommandPannel(commande));
-                count ++;
+            for (Commande commande : MonApplication.magasin().listerCommandesTerminees(context.client())) {
+                tabbedPane.addTab("order " + count, createCommandPannel(commande));
+                count++;
             }
             pack();
             setVisible(true);
